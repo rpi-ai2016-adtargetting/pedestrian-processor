a747b849325874628928d38a7093fc309ac4b09f
Pranav Sathyanarayanan <pranav.sathy@gmail.com>
2016-05-08 23:53:08 -0400

    Update to use HOG/LSVM at distance and face detection up close

--- realtime/detection.py
+++ realtime/detection.py
@@ -9,16 +9,27 @@ import cv2
 
 from camera import VideoCamera
 
+from PIL import Image
+
+def get_main_colors(col_list):
+    main_colors = set()
+    for index, color in col_list:
+        main_colors.add(tuple(component >> 6 for component in color))
+    return [tuple(component << 6 for component in color) for color in main_colors]
 
 # initialize the HOG descriptor/person detector
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+faceCascade = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
 
 cam = VideoCamera()
 
 while cam.video.isOpened():
 	#Get the image frame
 	image = cam.get_frame()
+	cv2_im = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+
 
 	image = imutils.resize(image, width=min(400, image.shape[1]))
 	orig = image.copy()
@@ -27,22 +38,35 @@ while cam.video.isOpened():
 	(rects, weights) = hog.detectMultiScale(image, winStride=(4, 4),
 		padding=(8, 8), scale=1.05)
 
-	# draw the original bounding boxes
-	# for (x, y, w, h) in rects:
-	# 	cv2.rectangle(orig, (x, y), (x + w, y + h), (0, 0, 255), 2)
-
-	# apply non-maxima suppression to the bounding boxes using a
-	# fairly large overlap threshold to try to maintain overlapping
-	# boxes that are still people
 	rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
 	pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
 
-	# draw the final bounding boxes
-	for (xA, yA, xB, yB) in pick:
-		cv2.rectangle(image, (xA, yA), (xB, yB), (0, 255, 0), 2)
+	if not len(pick):
+		gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+		faces = faceCascade.detectMultiScale(
+		    gray,
+		    scaleFactor=1.1,
+		    minNeighbors=5,
+		    minSize=(30, 30),
+		    flags = 0
+		)
+
+		for (x, y, w, h) in faces:
+			scale = w * h / 100;
+			xA = x - scale;
+			xB = x + scale + w;
+			yA = y - (scale / 2);
+			yB = y + (10 * scale)+ h;
+			cv2.rectangle(image, (xA, yA), (xB, yB), (0, 255, 0), 2)
+			#image = image[yA:yB, xA:xB]
+	else:
+		for (xA, yA, xB, yB) in pick:
+			cv2.rectangle(image, (xA, yA), (xB, yB), (0, 255, 0), 2)
+			#image = image[yA:yB, xA:xB]
 
 	# show the output images
-	cv2.imshow("After NMS", image)
+	cv2.imshow("Pedestrian", image)
 	if cv2.waitKey(5) & 0xFF == ord('q'):
  		break
 
